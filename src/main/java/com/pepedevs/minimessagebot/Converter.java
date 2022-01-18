5b4eba3307db72d97fc274683ced5478c6685143
RGoyalmc <76780190+RGoyalmc@users.noreply.github.com>
2022-01-18 23:42:21 +0530

    init repo

---
+++ src/main/java/com/pepedevs/minimessagebot/Converter.java
@@ -0,0 +1,37 @@
+package com.pepedevs.minimessagebot;
+
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.transformation.TransformationRegistry;
+import net.kyori.adventure.text.minimessage.transformation.TransformationType;
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+
+public class Converter {
+
+    private static final LegacyComponentSerializer LEGACY = LegacyComponentSerializer.builder().character('&').hexCharacter('#').build();
+    private static final MiniMessage MINI = MiniMessage.builder()
+            .transformations(TransformationRegistry.builder()
+                    .add(TransformationType.COLOR)
+                    .add(TransformationType.DECORATION)
+                    .add(TransformationType.HOVER_EVENT)
+                    .add(TransformationType.CLICK_EVENT)
+                    .add(TransformationType.KEYBIND)
+                    .add(TransformationType.TRANSLATABLE)
+                    .add(TransformationType.INSERTION)
+                    .add(TransformationType.FONT)
+                    .add(TransformationType.GRADIENT)
+                    .add(TransformationType.RAINBOW)
+                    .build())
+            .strict(false)
+            .build();
+    private static final GsonComponentSerializer GSON = GsonComponentSerializer.gson();
+
+    public static String fromLegacy(String input) {
+        return GSON.serialize(LEGACY.deserialize(input));
+    }
+
+    public static String fromMini(String input) {
+        return GSON.serialize(MINI.deserialize(input));
+    }
+
+}
---
+++ src/main/java/com/pepedevs/minimessagebot/Main.java
@@ -0,0 +1,9 @@
+package com.pepedevs.minimessagebot;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
